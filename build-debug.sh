2181f7d550ec42ce045cbb713482ec6ee9de1770
Martin Törnqvist <martin@Martin.(none)>
2018-06-18 18:26:39 +0200

    Windows cross compiling script and gitlab job

--- build-debug.sh
+++ build-debug.sh
@@ -7,6 +7,6 @@ root_dir=$PWD
 mkdir -p build
 cd build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
-make -j 4 ia-debug
+cmake --build . --target ia-debug -- -j 4
 
 cd $root_dir
--- build-release-windows-cross-compile.sh
+++ build-release-windows-cross-compile.sh
@@ -7,7 +7,7 @@ root_dir=$PWD
 rm -rf build
 mkdir build
 cd build
-cmake ..
+cmake -DWIN32=TRUE -DMSVC=FALSE -DARCH=64bit -DCMAKE_TOOLCHAIN_FILE=../Toolchain-cross-mingw32.txt ..
 cmake --build . --target install -- -j 4
 
 cd $root_dir
