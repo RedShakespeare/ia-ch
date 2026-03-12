fb99a87aace9c2cbb693ac96d4333e15efd22045
Martin Törnqvist <m.tornq@gmail.com>
2026-03-12 21:08:03 +0100

    Make Courageous into a War Vet unique trait again

--- src/player_bon.cpp
+++ src/player_bon.cpp
@@ -226,6 +226,7 @@ static void update_trait_data()
         d.id = TraitId::courageous;
         d.title = "Courageous";
         d.trait_prereqs = {TraitId::cool_headed};
+        d.bg_prereq = Bg::war_vet;
         set_trait_data(d);
 
         // --- Dexterous ---
