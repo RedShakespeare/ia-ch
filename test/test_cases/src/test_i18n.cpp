636644e58ab38d202a5e9ee89093a5597f0c6c77
Sil <sil@C20251126008091.local>
2026-06-19 18:34:46 +0000

    [i18n] Extract game command noise messages
    Route the make-noise log messages through game_commands i18n keys.
    
    Add zh_CN translations and i18n resolution coverage for the new keys.

--- test/test_cases/src/test_i18n.cpp
+++ test/test_cases/src/test_i18n.cpp
@@ -67,6 +67,12 @@ TEST_CASE("I18n loads Chinese UI strings and localized message files")
     REQUIRE(i18n::get("option.auto_reload_weapons.descr", "Automatically perform a reload action instead if attempting to fire a ranged weapon with no ammo loaded.") == "如果试图在没有装填弹药的情况下开火，则自动执行装填动作。");
     REQUIRE(i18n::get("terrain_mob.cough", "I cough.") == "我咳嗽起来。");
     REQUIRE(i18n::get("knockback.player_knocked_back", "I am knocked back!") == "我被击退了！");
+    REQUIRE(
+        i18n::get("game_commands.chilling_howl", "I let out a chilling howl.") ==
+        "我发出一声令人胆寒的嚎叫。");
+    REQUIRE(
+        i18n::get("game_commands.make_some_noise", "I make some noise.") ==
+        "我弄出一些声响。");
     REQUIRE(i18n::get("game_commands.press_help", "Press [?] for help.") == "按 [?] 查看帮助。");
     REQUIRE(i18n::get("msg_log.no_message_history", "No message history") == "没有消息历史");
     REQUIRE(
