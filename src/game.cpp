d18cebd65bbd8e2821f383cc4e5f3d8750e5258c
Sil <sil@C20251126008091.local>
2026-06-21 03:46:52 +0000

    [i18n] Extract game story messages
    Localize the intro story variants, default win narrative sections, and death message in the game state.
    
    Add zh_CN locale entries and i18n test coverage for the new game narrative keys.

--- src/game.cpp
+++ src/game.cpp
@@ -68,47 +68,69 @@ static int s_death_overlay_last_update_cycle = 0;
 
 static std::vector<HistoryEvent> s_history_events;
 
-static const std::string s_intro_msg_default =
-    "I stand at the end of a cobbled forest path, before me lies a shunned "
-    "and decrepit old church building. This is the access point to the "
-    "domains of the abhorred \"Cult of Starry Wisdom\". "
-    "I am determined to enter these sprawling catacombs and rob them of "
-    "treasures and knowledge. Somewhere below lies my true destiny, "
-    "an artifact of non-human origin referred to as "
-    "{COLOR_YELLOW}\"The shining Trapezohedron\"{reset_color} "
-    "- a window to all the secrets of the universe!";
+static std::string intro_msg_default()
+{
+    return i18n::get(
+        "game.intro_default",
+        "I stand at the end of a cobbled forest path, before me lies a shunned "
+        "and decrepit old church building. This is the access point to the "
+        "domains of the abhorred \"Cult of Starry Wisdom\". "
+        "I am determined to enter these sprawling catacombs and rob them of "
+        "treasures and knowledge. Somewhere below lies my true destiny, "
+        "an artifact of non-human origin referred to as "
+        "{COLOR_YELLOW}\"The shining Trapezohedron\"{reset_color} "
+        "- a window to all the secrets of the universe!");
+}
 
-static const std::string s_intro_msg_exorcist =
-    "I stand at the end of a cobbled forest path, before me lies a shunned "
-    "and decrepit old church building. This is the access point to the "
-    "domains of the abhorred \"Cult of Starry Wisdom\". "
-    "I am determined to enter these sprawling catacombs and purge them of "
-    "the corruption that dwells within. Somewhere below lies "
-    "an artifact of non-human origin referred to as "
-    "{COLOR_YELLOW}\"The shining Trapezohedron\"{reset_color} "
-    "- rumored to be a window to all the secrets of the universe. "
-    "This must be destroyed, so that none more may be tempted by "
-    "its deceitful promises!";
+static std::string intro_msg_exorcist()
+{
+    return i18n::get(
+        "game.intro_exorcist",
+        "I stand at the end of a cobbled forest path, before me lies a shunned "
+        "and decrepit old church building. This is the access point to the "
+        "domains of the abhorred \"Cult of Starry Wisdom\". "
+        "I am determined to enter these sprawling catacombs and purge them of "
+        "the corruption that dwells within. Somewhere below lies "
+        "an artifact of non-human origin referred to as "
+        "{COLOR_YELLOW}\"The shining Trapezohedron\"{reset_color} "
+        "- rumored to be a window to all the secrets of the universe. "
+        "This must be destroyed, so that none more may be tempted by "
+        "its deceitful promises!");
+}
 
-static const std::vector<std::string> s_win_msg_default = {
-    {"As I approach the crystal, an eerie glow illuminates the area. "
-     "I notice a figure observing me from the edge of the light. There is "
-     "no doubt in my mind concerning the nature of this entity."},
+static std::vector<std::string> win_msg_default()
+{
+    return {
+        i18n::get(
+            "game.win_default_approach_crystal",
+            "As I approach the crystal, an eerie glow illuminates the area. "
+            "I notice a figure observing me from the edge of the light. "
+            "There is no doubt in my mind concerning the nature of this "
+            "entity."),
 
-    {"I panic. Why is it I find myself here, stumbling around in "
-     "darkness? Is this all part of a plan? The being beckons me to "
-     "gaze into the stone."},
+        i18n::get(
+            "game.win_default_panic",
+            "I panic. Why is it I find myself here, stumbling around in "
+            "darkness? Is this all part of a plan? The being beckons me to "
+            "gaze into the stone."),
 
-    {"In the radiance I see visions beyond eternity, visions of "
-     "unreal reality, visions of the brightest light of day and the "
-     "darkest night of madness. There is only onward now, I have to see, "
-     "I have to KNOW."},
+        i18n::get(
+            "game.win_default_visions",
+            "In the radiance I see visions beyond eternity, visions of "
+            "unreal reality, visions of the brightest light of day and the "
+            "darkest night of madness. There is only onward now, I have to "
+            "see, I have to KNOW."),
 
-    {"So I make a pact with the Fiend."},
+        i18n::get(
+            "game.win_default_pact",
+            "So I make a pact with the Fiend."),
 
-    {"I now harness the shadows that stride from world to world to "
-     "sow death and madness. The destinies of all things on earth, "
-     "living and dead, are mine."}};
+        i18n::get(
+            "game.win_default_harness_shadows",
+            "I now harness the shadows that stride from world to world to "
+            "sow death and madness. The destinies of all things on earth, "
+            "living and dead, are mine.")};
+}
 
 static double mon_shock_lvl_to_shock_value(const MonShockLvl shock_lvl)
 {
@@ -456,11 +478,11 @@ void GameState::on_start()
 
             switch (player_bon::bg()) {
             case Bg::exorcist:
-                intro_msg = s_intro_msg_exorcist;
+                intro_msg = intro_msg_exorcist();
                 break;
 
             default:
-                intro_msg = s_intro_msg_default;
+                intro_msg = intro_msg_default();
                 break;
             }
 
@@ -628,7 +650,7 @@ void GameState::update()
         audio::play(audio::SfxId::death);
 
         msg_log::add(
-            "-I AM DEAD!-",
+            i18n::get("game.death_message", "-I AM DEAD!-"),
             colors::msg_bad(),
             MsgInterruptPlayer::no,
             MorePromptOnMsg::yes);
@@ -658,7 +680,7 @@ void WinGameState::draw()
 
     switch (player_bon::bg()) {
     default:
-        win_msg = s_win_msg_default;
+        win_msg = win_msg_default();
         break;
     }
 
